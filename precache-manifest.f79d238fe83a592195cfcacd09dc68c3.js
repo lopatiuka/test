18c11dc5088bcdce45430eb342ff0b2c76466927
alexander lopatiuk <alexander.lopatiuk@gmail.com>
2020-08-28 15:27:46 +0300

    Updates

---
+++ precache-manifest.f79d238fe83a592195cfcacd09dc68c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1d1c35f05f53b284e428c481064e40",
+    "url": "/remote-employees/index.html"
+  },
+  {
+    "revision": "cb1b6175eb118bb16bb4",
+    "url": "/remote-employees/static/css/main.332c4584.chunk.css"
+  },
+  {
+    "revision": "225f08ae084a7365a540",
+    "url": "/remote-employees/static/js/2.64d36fd5.chunk.js"
+  },
+  {
+    "revision": "e2f20cfd463376a4b4b55e3a97af5bf2",
+    "url": "/remote-employees/static/js/2.64d36fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1b6175eb118bb16bb4",
+    "url": "/remote-employees/static/js/main.2c3f2ed8.chunk.js"
+  },
+  {
+    "revision": "14097443eeb6f3f3a26f",
+    "url": "/remote-employees/static/js/runtime-main.ba295b99.js"
+  }
+]);
